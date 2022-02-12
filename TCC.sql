eea83c2296bcd185d83000ccb356665d1be64ea2
degu_cat <87346972+degu0@users.noreply.github.com>
2022-02-11 22:39:11 -0300

    Add files via upload

---
+++ TCC.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE TCC;
+USE TCC;
+
+CREATE TABLE Professor(
+	Nome varchar(255) NOT NULL,
+	AulasTrabalhadas int NOT NULL AUTO_INCREMENT,
+    AulasNaoTrabalhadas int NOT NULL, 
+    PRIMARY KEY (Nome),
+    CONSTRAINT Pr_Turma FOREIGN KEY (NomeDaTurma) REFERENCES Turma(NomeDaTurma),
+	CONSTRAINT Pr_Disciplina FOREIGN KEY (NomeDaDisciplina) REFERENCES Disciplinas(NomeDaDisciplina)
+)ENGINE = innodb;
+##Possivelmente essa columa não vai precisar usar
+## Colocar as horas "trabalhadas" de um professor
+
+CREATE TABLE Turma(
+	NomeDaTurma char(50) NOT NULL,
+    ApreviasaoDoNomeDaTurma char(35) NOT NULL, ##Opcional
+    Curso enum('MKT', 'TDS') NOT NULL, ## Coluna que tera so duas "resposta": Markenting (MKT) ou Desenvolvimento de Sistema(TDS)
+    PRIMARY KEY (NomeDaTurma),
+    CONSTRAINT TM_Disciplina FOREIGN KEY (NomeDaDisciplina) REFERENCES Disciplinas(NomeDaDisciplina)
+);
+
+CREATE TABLE Disciplinas(
+	NomeDaDisciplina char(50) NOT NULL,
+    ##Uma condição para que haja uma disciplima para uma turma espeficifica
+    AulaSemanais char(50) NOT NULL,
+    PRIMARY KEY (NomeDaDisciplina)
+);
+
+SELECT * FROM Disciplinas;
