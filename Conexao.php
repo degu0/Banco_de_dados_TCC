d1477cd224deba2950a3784acdd2a581df6f33c4
degu_cat <87346972+degu0@users.noreply.github.com>
2022-02-16 09:37:04 -0300

    Update Conexao.php

--- Conexao.php
+++ Conexao.php
@@ -7,6 +7,6 @@ $senha = "localhost";
 
 $mysqli = new mysqli($hostname, $bancodedados, $usuario, $senha);
 if ($mysqli->connect_errno) {
-    echo "Falha ao conectar: {" .$mysqli->connect_errno . ") ". $mysqli->connect_errno;
+    echo "Falha ao conectar: (" .$mysqli->connect_errno . ") ". $mysqli->connect_errno;
 }else 
     echo "Conectado!";
