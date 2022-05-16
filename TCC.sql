bee01b35b4051135f8f29973848c393e57993c76
dey_gu <87346972+degu0@users.noreply.github.com>
2022-05-16 00:23:20 -0300

    Update TCC.sql

--- TCC.sql
+++ TCC.sql
@@ -1,8 +1,17 @@
 CREATE DATABASE TCC;
 USE TCC;
 
+
+CREATE TABLE Login(
+	id int auto_increment,
+    Nome varchar(50) UNIQUE,
+    Senha varchar(50),
+    primary key(id) 
+);
+
 CREATE TABLE Disciplinas(
 	NomeDaDisciplina char(50) NOT NULL,
+    Ano int NOT NULL,
     ##Uma condição para que haja uma disciplima para uma turma espeficifica
     AulaSemanais char(50) NOT NULL,
     PRIMARY KEY (NomeDaDisciplina) 
@@ -10,6 +19,7 @@ CREATE TABLE Disciplinas(
 
 CREATE TABLE Turma(
 	NomeDaTurma char(50) NOT NULL,
+    Ano int NOT NULL,
     Curso enum('MKT', 'TDS') NOT NULL, ## Coluna que tera so duas "resposta": Markenting (MKT) ou Desenvolvimento de Sistema(TDS)
     PRIMARY KEY (NomeDaTurma)
 );
@@ -17,7 +27,6 @@ CREATE TABLE Turma(
 CREATE TABLE Professor(
 	Nome varchar(255) NOT NULL,
 	AulasTrabalhadas int NOT NULL, ##Possivelmente essa columa não vai precisar usar
-	AulasNaoTrabalhadas int NOT NULL, ##Possivelmente essa columa não vai precisar usar
 	NomeDaDisciplina char(50) NOT NULL,
 	NomeDaTurma char(50) NOT NULL,
 	PRIMARY KEY (Nome),
@@ -25,8 +34,8 @@ CREATE TABLE Professor(
 	CONSTRAINT Pr_Disciplina FOREIGN KEY (NomeDaDisciplina) REFERENCES Disciplinas(NomeDaDisciplina)
 );
 
-INSERT INTO Professor VALUES ("Tetheus", 26, 30, "PI3", "3_TDS_'B'");
-INSERT INTO Professor VALUES ("Matheus", 26, 30, "DAMO", "3_TDS_'B'");
+INSERT INTO Professor VALUES ("Matheus", 26, "PI3", "3_TDS_'B'");
+INSERT INTO Professor VALUES ("Matheus", 26,  "DAMO", "3_TDS_'B'");
 INSERT INTO Turma VALUES ("3_TDS_'B'", 'TDS');
 INSERT INTO Disciplinas VALUES ("DAMO", "Quinta-Feira");
 INSERT INTO Disciplinas VALUES ("PI3", "Quarta-Feira");
